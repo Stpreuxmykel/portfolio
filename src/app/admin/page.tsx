88ece307fc2575fffd3274b7eb73333e665a454c
Mike Nexus <94982836+Stpreuxmykel@users.noreply.github.com>
2024-10-15 23:50:02 -0400

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -8,7 +8,7 @@ const Admin = () => {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    axios.get('http://127.0.0.1:8000/api/contact/')
+    axios.get('https://portfolio310.pythonanywhere.com/api/contact/')
       .then(response => setItems(response.data))
       .catch(error => console.error(error))
   }, []);
